457bb5e2381dac76c091fb1aee5c879e45b82cc0
Jasper <jasper.lammers@gmail.com>
2025-01-28 09:28:27 +0100

    Implementation of initial extension methods

---
+++ src/NetDaemon.Extensions.Observables/BooleanObservableExtensions.Subscribe.cs
@@ -0,0 +1,37 @@
+﻿using Reactive.Boolean;
+
+namespace NetDaemon.Extensions.Observables
+{
+    public static class BooleanObservableExtensions
+    {
+        /// <summary>
+        /// Subscribes actions to be executed when the observable emits true (open) or false (closed).
+        /// </summary>
+        public static IDisposable SubscribeOpenClosed(this IObservable<bool> observable, Action openAction, Action closeAction) => observable.SubscribeTrueFalse(openAction, closeAction);
+
+        /// <summary>
+        /// Subscribes an action to be executed when the observable emits true (open).
+        /// </summary>
+        public static IDisposable SubscribeOpen(this IObservable<bool> observable, Action openAction) => observable.SubscribeTrue(openAction);
+
+        /// <summary>
+        /// Subscribes an action to be executed when the observable emits true (closed).
+        /// </summary>
+        public static IDisposable SubscribeClosed(this IObservable<bool> observable, Action closeAction) => observable.SubscribeFalse(closeAction);
+
+        /// <summary>
+        /// Subscribes actions to be executed when the observable emits true (on) or false (off).
+        /// </summary>
+        public static IDisposable SubscribeOnOff(this IObservable<bool> observable, Action onAction, Action offAction) => observable.SubscribeTrueFalse(onAction, offAction);
+
+        /// <summary>
+        /// Subscribes an action to be executed when the observable emits true (on).
+        /// </summary>
+        public static IDisposable SubscribeOn(this IObservable<bool> observable, Action onAction) => observable.SubscribeTrue(onAction);
+
+        /// <summary>
+        /// Subscribes an action to be executed when the observable emits false (off).
+        /// </summary>
+        public static IDisposable SubscribeOff(this IObservable<bool> observable, Action offAction) => observable.SubscribeTrue(offAction);
+    }
+}
---
+++ src/NetDaemon.Extensions.Observables/ObservableExtensions.Utils.cs
@@ -0,0 +1,40 @@
+﻿using System.Reactive.Linq;
+using NetDaemon.HassModel.Entities;
+
+namespace NetDaemon.Extensions.Observables
+{
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        /// Returns an observable that emits the same result as the source observable, but will repeat the last result when one of the provided entities becomes available.
+        /// </summary>
+        public static IObservable<T> RepeatWhenEntitiesBecomeAvailable<T>(this IObservable<T> observable,
+            params Entity[] entities) => InternalRepeatWhenEntitiesBecomeAvailable(observable, entities);
+
+        /// <summary>
+        /// Returns an observable that emits the same result as the source observable, but will repeat the last result when one of the provided entities becomes available.
+        /// </summary>
+        public static IObservable<T> RepeatWhenEntitiesBecomeAvailable<T>(this IObservable<T> observable,
+            IEnumerable<Entity> entities) => InternalRepeatWhenEntitiesBecomeAvailable(observable, entities);
+        
+        private static IObservable<T>
+            InternalRepeatWhenEntitiesBecomeAvailable<T>(
+                this IObservable<T> observable, IEnumerable<Entity> entities)
+        {
+            // Note: This might cause a duplicate emit if the source observable is not emitting before any of the entities go from unavailable to available.
+            var triggers = entities
+                .StateChanges()
+                .CombineLatest(observable)
+                .Where(values =>
+                    values.First.Old?.State != null &&
+                    values.First.New?.State != null &&
+                    Constants.EntityUnavailableStates.Contains(values.First.Old.State,
+                        StringComparer.OrdinalIgnoreCase) &&
+                    !Constants.EntityUnavailableStates.Contains(values.First.New.State,
+                        StringComparer.OrdinalIgnoreCase))
+                .Select(values => values.Second);
+
+            return observable.Merge(triggers);
+        }
+    }
+}
