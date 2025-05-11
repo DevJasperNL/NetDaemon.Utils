9d7dc05f4329e0cde8d936e2cd5148dec8938fc8
Jasper <jasper.lammers@gmail.com>
2025-05-11 10:42:25 +0200

    Implemented automation pipelines.

---
+++ src/AutomationPipelines/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AutomationPipelines.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCodeCasaPipeline(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .AddTransient(typeof(IPipeline<>), typeof(Pipeline<>))
+            .AddTransient(typeof(Pipeline<>), typeof(Pipeline<>));
+    }
+}
